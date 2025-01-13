ddd9a1ec887a0aeb1cc414718553ec31a350161a
Wang Xinglong <wangxinglong.123@bytedance.com>
2025-01-14 01:18:00 +0800

    update

--- biz/sal/rpc/miner_miner_core/raw_call.go
+++ biz/sal/rpc/miner_miner_core/raw_call.go
@@ -5,7 +5,8 @@ import (
 	"miner_api/biz/sal/rpc"
 
 	"github.com/cloudwego/kitex/client/callopt"
-	"github.com/qcq1/common/env"
+	"github.com/cloudwego/kitex/pkg/retry"
+	"github.com/qcq1/common/k8s_dns"
 	"github.com/qcq1/rpc_miner_core/kitex_gen/miner_core"
 
 	"github.com/bytedance/gopkg/util/logger"
@@ -17,26 +18,39 @@ import (
 var RawCall = NewRawCall()
 
 const (
-	ServiceName = "miner-core"
+	ServiceName    = "miner.miner-core"
+	BoeServiceName = "127.0.0.1:8889"
+	MaxRetryTimes  = 3
 )
 
 type RawCallStruct struct {
 	client minercore.Client
+	rp     *retry.FailurePolicy
 }
 
 func NewRawCall() *RawCallStruct {
 	r := &RawCallStruct{}
 	var err error
-	r.client, err = minercore.NewClient(rpc.Router[ServiceName][env.GetEnv()], client.WithResolver(dns.NewDNSResolver()), client.WithMiddleware(rpc.LogMiddleware))
+	r.client, err = minercore.NewClient(
+		k8s_dns.BuildK8sDestServiceName(
+			k8s_dns.WithDestServiceName(ServiceName),
+			k8s_dns.WithBoeDestServiceName(BoeServiceName),
+		),
+		client.WithResolver(dns.NewDNSResolver()),
+		client.WithMiddleware(rpc.LogMiddleware),
+	)
 	if err != nil {
 		logger.Errorf("minercore.NewClient failed, err: %v", err)
 		panic(err)
 	}
+	rp := retry.NewFailurePolicy()
+	rp.WithMaxRetryTimes(MaxRetryTimes)
+	r.rp = rp
 	return r
 }
 
-func (r *RawCallStruct) QueryJobList(ctx context.Context, req *miner_core.QueryJobListReq, callOptions ...callopt.Option) (resp *miner_core.QueryJobListResp, err error) {
-	resp, err = r.client.QueryJobList(ctx, req, callOptions...)
+func (r *RawCallStruct) QueryJobList(ctx context.Context, req *miner_core.QueryJobListReq) (resp *miner_core.QueryJobListResp, err error) {
+	resp, err = r.client.QueryJobList(ctx, req, callopt.WithRetryPolicy(retry.BuildFailurePolicy(r.rp)))
 	if err != nil {
 		logger.CtxErrorf(ctx, "client.QueryJobList failed, err: %v", err)
 		return
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/cloudwego/hertz v0.9.4
 	github.com/cloudwego/kitex v0.12.1
 	github.com/kitex-contrib/resolver-dns v0.1.0
-	github.com/qcq1/common v0.0.0-20250113160738-ebfb6234171c
+	github.com/qcq1/common v0.0.0-20250113165646-de03731bf0f9
 	github.com/qcq1/rpc_miner_core v0.0.0-20250112080543-67387b0a516f
 	google.golang.org/protobuf v1.33.0
 )
